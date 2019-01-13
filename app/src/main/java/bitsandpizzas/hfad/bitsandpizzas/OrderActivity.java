0166bbe9d376016a44fdfebc6b998ef304af9ce1
victor.arana <victor.arana@walmart.com>
2019-01-13 15:43:33 -0600

    Shows toolbar on Order activity

--- app/src/main/java/bitsandpizzas/hfad/bitsandpizzas/OrderActivity.java
+++ app/src/main/java/bitsandpizzas/hfad/bitsandpizzas/OrderActivity.java
@@ -2,6 +2,7 @@ package bitsandpizzas.hfad.bitsandpizzas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class OrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
