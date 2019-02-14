2baabc0dfb80f0894703f8f24e6b37584d55a7b8
victor.arana <victor.arana@walmart.com>
2019-02-14 11:12:05 -0600

    Fix commit

--- app/src/main/java/bitsandpizzas/hfad/bitsandpizzas/OrderActivity.java
+++ app/src/main/java/bitsandpizzas/hfad/bitsandpizzas/OrderActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -15,5 +16,9 @@ public class OrderActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void onClickDone(View view) {
+        // Code that runs when the Floating Action Button is clicked
     }
 }
