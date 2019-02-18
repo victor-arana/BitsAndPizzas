84324992acdab8ca1a35765d0425cf28ade69921
victor.arana <victor.arana@walmart.com>
2019-02-18 10:01:59 -0600

    Implements onBindViewHolder method.

--- app/src/main/java/bitsandpizzas/hfad/bitsandpizzas/CaptionedImagesAdapter.java
+++ app/src/main/java/bitsandpizzas/hfad/bitsandpizzas/CaptionedImagesAdapter.java
@@ -1,9 +1,13 @@
 package bitsandpizzas.hfad.bitsandpizzas;
 
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class CaptionedImagesAdapter extends RecyclerView.Adapter<CaptionedImagesAdapter.ViewHolder> {
 
@@ -16,12 +20,28 @@ public class CaptionedImagesAdapter extends RecyclerView.Adapter<CaptionedImages
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        return null;
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        CardView cardView =
+                (CardView) LayoutInflater
+                            .from(parent.getContext())
+                            .inflate(
+                                R.layout.card_captioned_image,
+                                parent,
+                                false);
+
+        return new ViewHolder(cardView);
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+        CardView cardView = viewHolder.cardView;
+        ImageView imageView = cardView.findViewById(R.id.info_image);
+        Drawable drawable =
+                ContextCompat.getDrawable(cardView.getContext(), imageIds[position]);
+        imageView.setImageDrawable(drawable);
+        imageView.setContentDescription(captions[position]);
+        TextView textView = cardView.findViewById(R.id.info_text);
+        textView.setText(captions[position]);
 
     }
 
