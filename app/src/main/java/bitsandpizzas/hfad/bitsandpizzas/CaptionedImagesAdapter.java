87aa20bece9e7b761f36665534b73867692f78d3
victor.arana <victor.arana@walmart.com>
2019-02-18 09:22:11 -0600

    Adds a recycler view adapter and specifies what sort of data to use.

---
+++ app/src/main/java/bitsandpizzas/hfad/bitsandpizzas/CaptionedImagesAdapter.java
@@ -0,0 +1,37 @@
+package bitsandpizzas.hfad.bitsandpizzas;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CaptionedImagesAdapter extends RecyclerView.Adapter<CaptionedImagesAdapter.ViewHolder> {
+
+    private String[] captions;
+    private int[] imageIds;
+
+    public CaptionedImagesAdapter(String[] captions, int[] imageIds) {
+        this.captions = captions;
+        this.imageIds = imageIds;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class ViewHolder  extends RecyclerView.ViewHolder{
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
