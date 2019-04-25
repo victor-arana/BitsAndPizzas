66c525f1e61b6785ec0012ba21c0bc8bf6d34ad0
victor.arana <victor.arana@walmart.com>
2019-04-25 11:08:04 -0500

    feature, adds single choice Dialog.

--- app/src/main/java/bitsandpizzas/hfad/bitsandpizzas/ExampleDialog.java
+++ app/src/main/java/bitsandpizzas/hfad/bitsandpizzas/ExampleDialog.java
@@ -8,17 +8,22 @@ import android.support.v7.app.AppCompatDialogFragment;
 
 public class ExampleDialog  extends AppCompatDialogFragment implements DialogInterface.OnClickListener {
 
+    String[] singleChoiceItems = {"Credit card", "Cash", "Coupon"};
+    int itemSelected = 0;
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("Information")
-                .setMessage("This is a Dialog")
-                .setPositiveButton("ok", this);
+        builder.setTitle("Payment Method")
+                .setSingleChoiceItems(singleChoiceItems, itemSelected, this)
+                .setPositiveButton("ok", null)
+                .setNegativeButton("Cancel", null);
         return builder.create();
     }
 
     @Override
     public void onClick(DialogInterface dialog, int which) {
-        //
+        // Do nothing
     }
 }
