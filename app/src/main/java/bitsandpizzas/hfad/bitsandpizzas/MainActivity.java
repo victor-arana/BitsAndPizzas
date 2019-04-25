cc4e1e5d05f6360f3bc9f6e4d5892b34ab9c6381
victor.arana <victor.arana@walmart.com>
2019-04-25 10:35:50 -0500

    feature, implements onClick handler.

--- app/src/main/java/bitsandpizzas/hfad/bitsandpizzas/MainActivity.java
+++ app/src/main/java/bitsandpizzas/hfad/bitsandpizzas/MainActivity.java
@@ -2,12 +2,31 @@ package bitsandpizzas.hfad.bitsandpizzas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+                          implements View.OnClickListener {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        openDialog();
+    }
+
+    private void openDialog() {
+        Toast.makeText(this, "To be implemented", Toast.LENGTH_LONG).show();
+
     }
 }
